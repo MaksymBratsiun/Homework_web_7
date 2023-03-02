af690f54b29a34687b4898d74960dad12e888250
MaksymBratsiun <bratsyun_m@ukr.net>
2023-03-02 11:12:52 +0200

    add interface to main, add my_select

--- main.py
+++ main.py
@@ -1,11 +1,117 @@
-import connect_db
+from pprint import pprint
+
+from sqlalchemy import inspect
+
+from connect_db import engine
 from seeds import seed_groups, seed_students, seed_teachers, seed_disciplines, seed_grades
+import my_select
+
+
+HELLO_TEXT = '''
+This DB created with Alembic.
+If you have existing tables in this DB and
+    you want to seed or use query input: "Y"
+If your DB is clear. Run in terminal
+    >>>"alembic upgrade head"
+    input: "N" for exit and creating DB'''
+
+QUERY_LIST = '''
+Choose any action and input number:
+0. Exit
+1. Знайти 5 студентів із найбільшим середнім балом з усіх предметів.
+2. Знайти студента із найвищим середнім балом з певного предмета.
+3. Знайти середній бал у групах з певного предмета.
+4. Знайти середній бал на потоці (по всій таблиці оцінок).
+5. Знайти які курси читає певний викладач.
+6. Знайти список студентів у певній групі.
+7. Знайти оцінки студентів у окремій групі з певного предмета.
+8. Знайти середній бал, який ставить певний викладач зі своїх предметів.
+9. Знайти список курсів, які відвідує студент.
+10. Список курсів, які певному студенту читає певний викладач.
+11. Середній бал, який певний викладач ставить певному студентові.
+12. Оцінки студентів у певній групі з певного предмета на останньому занятті.'''
+
+HANDLER_SELECT = {
+    '1': my_select.select_1,
+    '2': my_select.select_2,
+    '3': my_select.select_3,
+    '4': my_select.select_4,
+    '5': my_select.select_5,
+    '6': my_select.select_6,
+    '7': my_select.select_7,
+    '8': my_select.select_8,
+    '9': my_select.select_9,
+    '10': my_select.select_10,
+    '11': my_select.select_11,
+    '12': my_select.select_12
+}
+
+
+def get_handler(operator):
+    return HANDLER_SELECT.get(operator)
+
+
+def db_exists():
+    return (inspect(engine).has_table("students") and
+            inspect(engine).has_table("groups") and
+            inspect(engine).has_table("teachers") and
+            inspect(engine).has_table("disciplines") and
+            inspect(engine).has_table("grades"))
+
+
+def seed_all():
+    try:
+        seed_teachers()
+        seed_groups()
+        seed_students()
+        seed_disciplines()
+        seed_grades()
+    except Exception as err:
+        print(err)
 
 
 if __name__ == '__main__':
-    seed_teachers()
-    seed_groups()
-    seed_students()
-    seed_disciplines()
-    seed_grades()
+    print(HELLO_TEXT)
+    user_input = ''
+    db_has_tables = db_exists()
+    while True:
+        user_input = input('Do you create empty BD tables? (Y/N): ')
+        user_input = user_input.strip().lower()
+        if user_input == 'y':
+            if db_has_tables:
+                print('You have correct DB')
+                break
+            else:
+                print('Your DB has wrong structure. Exit and create new DB.')
+        elif user_input == 'n':
+            exit()
+
+    while True:
+        user_input = input('Do you want to seed tables? (Y/N): ')
+        user_input = user_input.strip().lower()
+        db_has_info = my_select.select_seeded()
+        if user_input == 'y':
+            if not db_has_info:
+                seed_all()
+                print('Seed DB completed')
+            else:
+                print('DB has info.\nExit, clear tables and make new seed \nor continue with this info')
+        elif user_input == 'n':
+            break
+
+    print(QUERY_LIST)
+
+    while True:
+        user_input = input('Input action?(0-12): ')
+        user_input = user_input.strip().lower()
+        if user_input == '0':
+            break
+        if user_input in HANDLER_SELECT:
+            try:
+                str_query = str(get_handler(user_input)())
+                print(str_query)
+            except FileNotFoundError as e:
+                print(e)
+        else:
+            print(f'Incorrect input: {user_input}. Try again...')
 
--- my_select.py
+++ my_select.py
@@ -1,14 +1,190 @@
-from sqlalchemy import func
+from sqlalchemy import func, desc, and_
 
 from connect_db import session
-from models import Student, Group
+from models import Student, Group, Discipline, Teacher, Grade
 
 
-def select_test():
-    result = session.query(Student.fullname, Group.name).select_from(Student).join(Group).filter(Group.id == 1).all()
-    for row in result:
-        print(row.fullname, row.name)
+def select_seeded():
+    has_student = session.query(Student.id).select_from(Student).filter(Student.id == 1).first()
+    has_group = session.query(Group.id).select_from(Group).filter(Group.id == 1).first()
+    has_discipline = session.query(Discipline.id).select_from(Discipline).filter(Discipline.id == 1).first()
+    has_teacher = session.query(Teacher.id).select_from(Teacher).filter(Teacher.id == 1).first()
+    has_grade = session.query(Grade.id).select_from(Grade).filter(Grade.id == 1).first()
+    if (has_grade and
+            has_student and
+            has_discipline and
+            has_teacher and
+            has_group):
+        return True
 
 
-if __name__ == '__main__':
-    select_test()
+def select_1():
+    result_list = []
+    res_query = session.query(Student.fullname,
+                              func.round(func.avg(Grade.grade), 2).label('avg_grade')) \
+        .select_from(Grade) \
+        .join(Student)\
+        .group_by(Student.id) \
+        .order_by(desc('avg_grade')) \
+        .limit(5).all()
+    for row in res_query:
+        result_list.append(f'{row.fullname}: {row.avg_grade}')
+    return '\n'.join(result_list)
+
+
+def select_2():
+    res_query = session.query(Discipline.name,
+                              Student.fullname,
+                              func.round(func.avg(Grade.grade), 2).label('avg_grade')
+                              ) \
+        .select_from(Grade) \
+        .join(Student) \
+        .join(Discipline) \
+        .filter(Discipline.id == 1) \
+        .group_by(Student.id, Discipline.name) \
+        .order_by(desc('avg_grade')) \
+        .limit(1).all()
+    return f'{res_query[0][0]} - {res_query[0][1]}: {res_query[0][2]}'
+
+
+def select_3():
+    result_list = []
+    res_query = session.query(Discipline.name,
+                              Group.name,
+                              func.round(func.avg(Grade.grade), 2).label('avg_grade')
+                              ) \
+        .select_from(Grade) \
+        .join(Student) \
+        .join(Discipline) \
+        .join(Group) \
+        .filter(Discipline.id == 1) \
+        .group_by(Discipline.name, Group.name) \
+        .order_by(Group.name) \
+        .all()
+    for i in range(len(res_query)):
+        result_list.append(f'{res_query[i][0]} - {res_query[i][1]}: {res_query[i][2]}')
+    return '\n'.join(result_list)
+
+
+def select_4():
+    res_query = session.query(func.round(func.avg(Grade.grade), 2).label('avg_grade')) \
+        .limit(1).all()
+    return f'Average grade: {res_query[0][0]}'
+
+
+def select_5():
+    result_list = []
+    res_query = session.query(Discipline.name,
+                              Teacher.fullname
+                              ) \
+        .select_from(Discipline) \
+        .join(Teacher) \
+        .filter(Teacher.id == 1) \
+        .group_by(Discipline.name, Teacher.fullname) \
+        .all()
+    for i in range(len(res_query)):
+        result_list.append(f'{res_query[i][1]} - {res_query[i][0]}')
+    return '\n'.join(result_list)
+
+
+def select_6():
+    result_list = []
+    res_query = session.query(Group.name,
+                              Student.fullname
+                              ) \
+        .select_from(Student) \
+        .join(Group) \
+        .filter(Group.id == 1) \
+        .group_by(Group.name, Student.fullname) \
+        .all()
+    for i in range(len(res_query)):
+        result_list.append(f'{res_query[i][0]} - {res_query[i][1]}')
+    return '\n'.join(result_list)
+
+
+def select_7():
+    result_list = []
+    res_query = session.query(Group.name,
+                              Discipline.name,
+                              Student.fullname,
+                              Grade.grade,
+                              ) \
+        .select_from(Grade) \
+        .join(Student) \
+        .join(Discipline) \
+        .join(Group) \
+        .filter(and_(Group.id == 1, Grade.discipline_id == 1)) \
+        .all()
+    for i in range(len(res_query)):
+        result_list.append(f'{res_query[i][0]}, {res_query[i][1]} - {res_query[i][2]}: {res_query[i][3]}')
+    return '\n'.join(result_list)
+
+
+def select_8():
+    res_query = session.query(Teacher.fullname,
+                              func.round(func.avg(Grade.grade), 2).label('avg_grade')
+                              ) \
+        .select_from(Grade) \
+        .join(Discipline) \
+        .join(Teacher) \
+        .filter(Teacher.id == 1) \
+        .group_by(Teacher.fullname) \
+        .all()
+    return f'{res_query[0][0]}: {res_query[0][1]}'
+
+
+def select_9():
+    result_list = []
+    res_query = session.query(Discipline.name,
+                              Student.fullname,
+                              ) \
+        .select_from(Grade) \
+        .join(Student) \
+        .join(Discipline) \
+        .filter(Student.id == 1)\
+        .group_by(Student.id, Discipline.name) \
+        .all()
+    for i in range(len(res_query)):
+        result_list.append(f'{res_query[i][1]} - {res_query[i][0]}')
+    return '\n'.join(result_list)
+
+
+def select_10():
+    result_list = []
+    res_query = session.query(Discipline.name.label('discipline'),
+                              Student.fullname.label('student'),
+                              Teacher.fullname.label('teacher'),
+                              ) \
+        .select_from(Grade) \
+        .join(Student) \
+        .join(Discipline) \
+        .join(Teacher) \
+        .filter(and_(Student.id == 1, Teacher.id == 1))\
+        .group_by(Student.fullname, Discipline.name, Teacher.fullname) \
+        .all()
+    for i in range(len(res_query)):
+        result_list.append(f'{res_query[i][0]}: {res_query[i][1]} - {res_query[i][2]}')
+    return '\n'.join(result_list)
+
+
+def select_11():
+    res_query = session.query(Student.fullname.label('student'),
+                              Teacher.fullname.label('teacher'),
+                              func.round(func.avg(Grade.grade), 2).label('avg_grade')
+                              ) \
+        .select_from(Grade) \
+        .join(Student) \
+        .join(Discipline) \
+        .join(Teacher) \
+        .filter(and_(Student.id == 1, Teacher.id == 1)) \
+        .group_by(Teacher.fullname, Student.fullname) \
+        .all()
+    return f'{res_query[0][1]} - {res_query[0][0]} :{res_query[0][2]}'
+
+
+def select_12():
+    return 'Sorry. This chapter is under work.'
+
+
+# if __name__ == '__main__':
+#     print(select_11())
